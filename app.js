84547fa2fb776e4ea8d0bce227e0c6f4ccb35b6f
kreischmura <karina.chmura@escola.pr.gov.br>
2025-05-26 18:13:17 -0300

    criando base das perguntas e respostas

---
+++ app.js
@@ -0,0 +1,16 @@
+const flashcards = [
+    { question: "Quem é o pai do Naruto?", answer: "Minato Namikaze (4º Hokage)" },
+    { question: "Qual o nome do time de Naruto?", answer: "Time 7" },
+    { question: "Quem é o mestre do Kakashi?", answer: "Minato Namikaze" },
+    { question: "Quem foi o primeiro Hokage?", answer: "Hashirama Senju" },
+    { question: "Qual o nome da besta de 9 caudas?", answer: "Kurama" },
+    { question: "Quem mata o Itachi?", answer: "Sasuke Uchiha" },
+    { question: "Qual o clã do Neji?", answer: "Clã Hyuuga" },
+    { question: "Quem é o Jinchuuriki do Shukaku?", answer: "Gaara" },
+    { question: "Quem é o irmão do Sasuke?", answer: "Itachi Uchiha" },
+    { question: "Qual é a Kekkei Genkai do Haku?", answer: "Hyoton (Elemento Gelo)" },
+    { question: "Qual o nome do vilarejo do Naruto?", answer: "Konohagakure (Aldeia da Folha)" },
+    { question: "Quem é o líder da Akatsuki?", answer: "Pain (Nagato)" }
+  ];
+  
+  const cardGrid = document.getElementById("cardGrid");
